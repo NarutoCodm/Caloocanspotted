2a14df4c8f77391f118f15bb488976a6376daea1
NarutoGaming <88095348+NarutoCodm@users.noreply.github.com>
2022-10-01 10:36:39 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+<div>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Caloocan Spotted</title>
+    </head>
+    <body>
+        <p>omsim</p>
+    </body>
+    </html>
+    <img src="https://cdn.discordapp.com/attachments/1023593531098529915/1023610383371620462/mqdefault.jpg" alt="">
+    <li>Eh, paano kung hindi
+Hindi ka nakilala?
+Siguro, nakakulong pa din
+Sa nakaraan, 'di makalaya</li>
+<audio controls>
+  <source src="horse.mp3" type="audio/mpeg">
+</source></audio>
+<link rel="stylesheet" href="https://cdn.discordapp.com/attachments/1023593531098529915/1023610383371620462/mqdefault.jpg">
+
+
+</div>
